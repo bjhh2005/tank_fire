9c44eb2b70bb32857882a643b8c404fb3f726d96
ywtshuai <2287943335@qq.com>
2024-07-05 23:35:54 +0800

    ywt的test2

---
+++ !置顶!开发内容/ywt的test2/Source/test2/Public/WeaponBullet.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WeaponBullet.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+
+UCLASS()
+class TEST2_API AWeaponBullet : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWeaponBullet();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	USphereComponent* SphereCollision;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UProjectileMovementComponent* MovementComponent;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
