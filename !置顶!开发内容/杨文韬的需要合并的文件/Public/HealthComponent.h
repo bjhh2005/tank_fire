eae4eae8b4a389da9286f64a46ed1398ddde061b
ich-liebe-dich-Elysia <3280354053@qq.com>
2024-07-08 17:33:02 +0800

    111

---
+++ !置顶!开发内容/杨文韬的需要合并的文件/Public/HealthComponent.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TANK_FIRE_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UHealthComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
+	float maxhealth=100;
+
+private:
+	float health;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	float GetHealth() { return health; }
+
+	UFUNCTION(BlueprintCallable)
+	void TakeDamage(float amount);
+};
